b88b98be4b63eb40b6fcc5983f6683ba127b85a5
Anthony Angelo <angelo_anthony@bah.com>
2019-11-04 18:31:20 -0500

    Update

--- data/rest.php
+++ data/rest.php
@@ -19,7 +19,7 @@ if ($curl_response === false) {
 }
 curl_close($curl);
 
-$items = json_decode($response, true);  
+$response = json_decode($response, true);  
 
 $_SESSION['item'] = array();
 
@@ -27,7 +27,7 @@ $_SESSION['item'] = array();
 $selector = $_SESSION['SELECTOR'];
 $i = 0;
 
-foreach ($items as $item) {
+foreach ($response as $item) {
 
     $id = $item['Item']['id'];
     $name = $item['Item']['name'];
